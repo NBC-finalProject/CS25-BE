2a0f547ec9d5c1c39b18f4e2bdca956b100d7290
crocusia <rainbowsubin0522@gmail.com>
2025-08-26 20:04:16 +0900

    feat : 누락된 코드 추가

--- cs25-batch/src/main/java/com/example/cs25batch/sender/MailSenderStrategy.java
+++ cs25-batch/src/main/java/com/example/cs25batch/sender/MailSenderStrategy.java
@@ -6,5 +6,7 @@ import io.github.bucket4j.Bucket;
 public interface MailSenderStrategy {
     void sendQuizMail(MailDto mailDto);
 
-    Bucket getBucket();
+    boolean tryConsume(Long num);
+
+    void acquirePermitOrWait();
 }
